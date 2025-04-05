43f77c277eb2cd3252198cca27a21d8e75f71bac
Fernando Coronel <fcoronel@gmail.com>
2025-04-04 22:53:18 -0300

    Practica 5 - Punto 1

---
+++ practica5/API/Inc/API_uart.h
@@ -0,0 +1,40 @@
+/*
+ * API_uart.h
+ *
+ *  Created on: Apr 3, 2025
+ *      Author: fcoronel
+ */
+
+#ifndef INC_API_UART_H_
+#define INC_API_UART_H_
+
+#include "API_delay.h"
+
+/**
+ * @brief inicializa el UART, en particular el UART2
+ */
+bool_t uartInit();
+
+/**
+ * @brief Envia un byte por el UART
+ * @param byte: byte a enviar
+ * @retVal: none
+ */
+void uartSendString(uint8_t * pstring);
+
+/**
+ * @brief Recibe un byte por el UART
+ * @param byte: puntero a la variable donde se guardara el byte recibido
+ * @retVal: none
+ */
+void uartSendStringSize(uint8_t * pstring, uint16_t size);
+
+/**
+ * @brief Recibe un byte por el UART
+ * @param byte: puntero a la variable donde se guardara el byte recibido
+ * @retVal: none
+ */
+void uartReceiveStringSize(uint8_t * pstring, uint16_t size);
+
+
+#endif /* INC_API_UART_H_ */
---
+++ practica5/API/Src/API_uart.c
@@ -0,0 +1,52 @@
+/*
+ * API_uart.c
+ *
+ *  Created on: Apr 3, 2025
+ *      Author: fcoronel
+ */
+
+#include "API_uart.h"
+#include "stm32f4xx_hal.h"
+#include <string.h>
+
+extern UART_HandleTypeDef huart2;
+
+const uint32_t UART_Timeout = 1000; /**< Timeout para la transmision de datos por UART. */
+
+bool_t uartInit() {
+	  huart2.Instance = USART2;
+	  huart2.Init.BaudRate = 115200;
+	  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	  huart2.Init.StopBits = UART_STOPBITS_1;
+	  huart2.Init.Parity = UART_PARITY_NONE;
+	  huart2.Init.Mode = UART_MODE_TX_RX;
+	  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart2) == HAL_OK) {
+      uartSendString((uint8_t *)"baud rate: 115200\r\n");
+      uartSendString((uint8_t *)"word len: 8bits\r\n");
+      uartSendString((uint8_t *)"stop bits: 1\r\n");
+      uartSendString((uint8_t *)"parity: NONE\r\n");
+      return true;
+    } else {
+      return false;
+    }
+
+}
+
+void uartSendString(uint8_t * pstring) {
+  assert_param(pstring != NULL);
+	uint16_t len = strlen((char*) pstring);
+	HAL_UART_Transmit(&huart2, pstring, len, UART_Timeout);
+}
+
+
+void uartSendStringSize(uint8_t * pstring, uint16_t size) {
+  assert_param(pstring != NULL);
+	HAL_UART_Transmit(&huart2, pstring, size, UART_Timeout);
+}
+
+void uartReceiveStringSize(uint8_t * pstring, uint16_t size) {
+  assert_param(pstring != NULL);
+  assert_param(size > 0);
+}
